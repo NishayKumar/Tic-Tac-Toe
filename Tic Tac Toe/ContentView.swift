0ac741ec25c64ffebbee6b7cd9057fb68da5925b
Nishaykumar <Nishaykumar24@gmail.com>
2023-07-25 23:23:57 +0530

    Initial commit

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    let column: [GridItem] = [GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{ geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: column, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.pink)
+                                .opacity(colorScheme == .dark ? 1 : 0.7)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(colorScheme == .dark ? Color.black : Color.white)
+                            
+                        }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 

11c63dfb2c4d4581b02801ca548f1eedc8cbf79f
Nishaykumar <Nishaykumar24@gmail.com>
2023-09-22 12:43:23 +0530

    test: CheckForDraw unit testing implemented

--- Tic Tac Toe Tests/Tic_Tac_Toe_Tests.swift
+++ Tic Tac Toe Tests/Tic_Tac_Toe_Tests.swift
@@ -6,30 +6,31 @@
 //
 
 import XCTest
+@testable import Tic_Tac_Toe
 
 final class Tic_Tac_Toe_Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: GameViewModel!
+    func testCheckForDraw_AllNil() {
+        // Arrange
+        let moves: [Move?] = Array(repeating: nil, count: 9)
+        
+        // Act
+        let isDraw = viewModel.checkForDraw(in: moves)
+        
+        // Assert
+//        XCTAssertTrue(isDraw, "Expected a draw when all elements are nil")
+        XCTAssertNil(isDraw)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testCheckForDraw_NotAllNil() {
+        // Arrange
+        let moves: [Move?] = [nil, Move(player: .human, boardIndex: 1), nil, Move(player: .computer, boardIndex: 3), nil, nil, nil, Move(player: .human, boardIndex: 7), Move(player: .computer, boardIndex: 8)]
+        let viewModel = GameViewModel()
+        
+        // Act
+        let isDraw = viewModel.checkForDraw(in: moves)
+        
+        // Assert
+        XCTAssertFalse(isDraw, "Expected not a draw when not all elements are nil")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

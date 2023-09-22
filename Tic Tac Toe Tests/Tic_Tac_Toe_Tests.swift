3681f98b53107693e70aafb4fb2f5e9c705b0c74
Nishaykumar <Nishaykumar24@gmail.com>
2023-09-22 19:15:11 +0530

    test: Add test case for the checkForDraw function

--- Tic Tac Toe Tests/Tic_Tac_Toe_Tests.swift
+++ Tic Tac Toe Tests/Tic_Tac_Toe_Tests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Tic_Tac_Toe
 
 final class Tic_Tac_Toe_Tests: XCTestCase {
-    var viewModel: GameViewModel!
+    let viewModel = GameViewModel()
     func testCheckForDraw_AllNil() {
         // Arrange
         let moves: [Move?] = Array(repeating: nil, count: 9)
@@ -18,14 +18,12 @@ final class Tic_Tac_Toe_Tests: XCTestCase {
         let isDraw = viewModel.checkForDraw(in: moves)
         
         // Assert
-//        XCTAssertTrue(isDraw, "Expected a draw when all elements are nil")
-        XCTAssertNil(isDraw)
+        XCTAssertFalse(isDraw, "Expected not a draw when all elements are nil")
     }
     
     func testCheckForDraw_NotAllNil() {
         // Arrange
         let moves: [Move?] = [nil, Move(player: .human, boardIndex: 1), nil, Move(player: .computer, boardIndex: 3), nil, nil, nil, Move(player: .human, boardIndex: 7), Move(player: .computer, boardIndex: 8)]
-        let viewModel = GameViewModel()
         
         // Act
         let isDraw = viewModel.checkForDraw(in: moves)
@@ -33,4 +31,19 @@ final class Tic_Tac_Toe_Tests: XCTestCase {
         // Assert
         XCTAssertFalse(isDraw, "Expected not a draw when not all elements are nil")
     }
+    
+    func testCheckForDraw_NoNil() {
+        // Arrange
+        let moves: [Move?] = [Move(player: .human, boardIndex: 0), Move(player: .human, boardIndex: 1), Move(player: .human, boardIndex: 2), Move(player: .computer, boardIndex: 3), Move(player: .human, boardIndex: 4), Move(player: .human, boardIndex: 5), Move(player: .human, boardIndex: 6), Move(player: .human, boardIndex: 7), Move(player: .computer, boardIndex: 8)]
+        
+        // Act
+        let isDraw = viewModel.checkForDraw(in: moves)
+        
+        // Assert
+        XCTAssertTrue(isDraw, "Expected a draw when no elements are nil")
+    }
 }
+
+
+
+
